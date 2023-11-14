2a5b8a7bfa4164a5c482353a3a7d2e0b9e678633
Codebear <thorbjorn.hoib@hotmail.com>
2023-11-14 13:21:23 +0100

    Add project files.

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace GuessTheNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat = true;
+            while (repeat == true)
+            {
+                PlayGame();
+                var answer = Console.ReadLine();
+                if(answer != "Y") repeat = false;
+            }
+        }
+
+        private static void PlayGame()
+        {
+            var rand = new Random();
+            var randomNumber = rand.Next(1, 101);
+            Console.WriteLine("Gjett et tilfeldig tall (1 - 100)");
+            var userGuess = Convert.ToInt32(Console.ReadLine());
+
+            while (userGuess != randomNumber)
+            {
+                if (userGuess < randomNumber)
+                {
+                    Console.WriteLine("Høyere!");
+                }
+                else if (userGuess > randomNumber)
+                {
+                    Console.WriteLine("Lavere!");
+                }
+                
+                userGuess = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("You win! Replay? Y/N");
+        }
+    }
+}
